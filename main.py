468c8e3fe63c4bb0e5bc6dfdc44f0ed9d68848bb
Ouasfi <amine1ouasfi@hotmail.f>
2021-03-03 11:25:56 +0100

    add base url and filterPattern

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import scrape
 import process
 import argparse
-def FindJobs(jobTitle : str, jobType :str):
+def FindJobs(jobTitle : str, jobType :str, filterPattern: str):
+    base_url = "https://www.indeed.fr"
     url = scrape.get_url(base_url, jobTitle,jobType)
     df = scrape.get_offers(base_url,url)
-    df_pr = process.process_offers(df,r'Data')
+    df_pr = process.process_offers(df,r'filterPattern')
     df_pr.to_csv('offers.csv')
     return df_pr
 if __name__ == "__main__":
@@ -15,5 +16,5 @@ if __name__ == "__main__":
     base_url = "https://www.indeed.fr"
     job = args.job
     jt = args.type
-    df_pr = FindJobs(jobTitle = job, jobType = jt)
+    df_pr = FindJobs(jobTitle = job, jobType = jt, filterPattern='sklearn')
     print(len(df_pr))
